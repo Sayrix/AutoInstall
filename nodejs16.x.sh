4dfa46f51d73d6103cbb700d6c04f5d847a52d92
Sayrix <43046854+Sayrix@users.noreply.github.com>
2021-06-27 19:23:18 +0200

    Update nodejs16.x.sh

--- nodejs16.x.sh
+++ nodejs16.x.sh
@@ -1,5 +1,5 @@
 cd ~
-curl -sL https://deb.nodesource.com/setup_15.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs -y
 sudo rm -rf nodesource_setup.sh
