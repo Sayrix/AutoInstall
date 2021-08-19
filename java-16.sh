8981806883e33aa22cd911f03e3c50324fff6d43
Sayrix <43046854+Sayrix@users.noreply.github.com>
2021-08-19 02:22:34 +0200

    Update java-16.sh

--- java-16.sh
+++ java-16.sh
@@ -2,4 +2,5 @@ cd ~
 echo "deb http://ppa.launchpad.net/linuxuprising/java/ubuntu focal main" | tee /etc/apt/sources.list.d/linuxuprising-java.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A
 sudo apt-get update -y
+apt-get install java -y
 java -version
