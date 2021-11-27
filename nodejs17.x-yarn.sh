b5ad82d55cdb729ca78b2914f8c9909a98ca3d0c
Sayrix <43046854+Sayrix@users.noreply.github.com>
2021-11-27 18:04:56 +0100

    Update and rename nodejslts-yarn.sh to nodejs17.x-yarn.sh

--- nodejs17.x-yarn.sh
+++ nodejs17.x-yarn.sh
@@ -1,6 +1,7 @@
 cd ~
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_17.x | sudo bash -
 sudo apt -y install nodejs -y
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt install yarn -y
+node -v
