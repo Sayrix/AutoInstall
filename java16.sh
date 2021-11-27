6ad50ac2e29b411eb8163ec8ab5a503fca9d89ce
Sayrix <43046854+Sayrix@users.noreply.github.com>
2021-11-27 18:17:04 +0100

    Update java16.sh

--- java16.sh
+++ java16.sh
@@ -1,4 +1,4 @@
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository --yes ppa:rpardini/adoptopenjdk
-sudo apt-get update -y
-sudo apt-get install adoptopenjdk-16-installer 
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+sudo apt update
+apt install adoptopenjdk-16-hotspot
